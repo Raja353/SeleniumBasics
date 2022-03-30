7e8c07c2d72cdbe8693a8affb8dad47125ae2abb
Raja353 <drajasekarmca@gmail.com>
2022-03-30 16:25:42 +0530

    my develpment commit

---
+++ src/development/Development.java
@@ -0,0 +1,11 @@
+package development;
+
+public class Development {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is development class");
+
+	}
+
+}
